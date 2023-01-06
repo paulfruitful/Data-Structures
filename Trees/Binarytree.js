83e7e29ec8a590e388e8854c3fe92e89deb262e1
Paul Fruitful <fruitful2007@outlook.com>
2023-01-06 21:27:12 +0100

    Third commit

--- Trees/Binarytree.js
+++ Trees/Binarytree.js
@@ -1,6 +1,15 @@
+class node{
+    constructor(root,left,right){
+        this.root=root
+        this.left=left
+        this.right=right
+    }
+}
+
+
 class BinaryTree{
-  constructor(){
-    
+  constructor(root){
+    this.root=root
   }
 
 }
