48e821ea614613aa24eb7e42d741c229cf59e38b
Paul Fruitful <fruitful2007@outlook.com>
2023-01-07 22:28:36 +0100

    Adding elements

--- Trees/MaxHeap.js
+++ Trees/MaxHeap.js
@@ -20,6 +20,21 @@ rightchild(index)
 {
     (2*index)+2
 }
+
+exchange(i1,i2){
+    this.values[i1],this.values[i2]=this.values[i2],this.values[i1]
+}
+add(node) {
+    // add element to the end of the heap
+    this.values.push(element);
+    // move element up until it's in the correct position
+    this.Up(this.values.length - 1);
+}
+Up(index){
+    let nowIndex=index
+    let parentIndex=this.parent(nowIndex)
+    
+}
   }
   
 
