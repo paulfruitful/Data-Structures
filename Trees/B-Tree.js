c956aa5d95dd40a46cc7619dfd04047a89f12fde
Paul Fruitful <fruitful2007@outlook.com>
2023-01-25 21:54:18 +0100

    B-Tree: insert Node

--- Trees/B-Tree.js
+++ Trees/B-Tree.js
@@ -5,10 +5,17 @@ class Btree{
  insertNode(node){
     if (this.root.length==0){
         this.root[node]={}
-    }else if(this.root.length<3){
+    }else if(this.root.length<3 && !node in this.root){
         this.root[node]={}
     }else{
-        
+
     }
  }
 }
+
+
+
+let test= new Btree()
+
+test.insertNode(5)
+test.insertNode(3)
