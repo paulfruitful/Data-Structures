5ec9f3dd3af2fd0db439d42017d4397ab94e05d1
Paul Fruitful <fruitful2007@outlook.com>
2023-03-28 23:07:26 +0100

    Dynamic Window: Finding the Max Repeating SubString of a String

---
+++ Sliding Window Algorithm/dyamicWindow-string.py
@@ -0,0 +1,20 @@
+def findMaxRepeatSubString(string,k):
+    maxString=float('inf')
+    add=0
+    start=0
+    subString=set()
+    currentString=''
+    for i in range(len(string)):
+       currentString+=string[i]
+       if (i-start)==k-1:
+           if currentString in subString:
+               subString.pop(currentString)
+               maxString=max(maxString,add+1)
+           else:
+               subString.add(currentString)
+               start+=1
+    return maxString
+
+
+test='AAAAAAAAAAAAAAAAAAABBBCCIMMSMSSIIIIIIIAAAAAAAAAIIDNNDNNAAAAAAAAAAHDDDHJAAAAAAAAAAAA'
+print(findMaxRepeatSubString(test,5))
