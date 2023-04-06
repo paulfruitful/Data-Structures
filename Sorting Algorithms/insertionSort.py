1b224de006b9c8dc77102d8548ee0158059fbc41
Paul Fruitful <fruitful2007@outlook.com>
2023-04-06 09:57:01 +0100

    Insertioon sort decreasing order

--- Sorting Algorithms/insertionSort.py
+++ Sorting Algorithms/insertionSort.py
@@ -9,6 +9,17 @@ def insertSort(array):
       array[prev+1]=current
     return array
 
+def insertSort_dec(array):
+
+    for i in range(1, len(array)):
+      current=array[i]
+      prev=i-1
+      while prev>=0 and array[prev] <current:
+         array[i+1]= array[prev]
+         prev-=1
+      array[prev+1]=current
+    return array
+
 
 a=[6,8,3,2,1,20,14,35,67,23,7]
 
