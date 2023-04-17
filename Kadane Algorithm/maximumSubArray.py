cee1d2edb21d40bcc793236d2b8b354cc02aebf5
Paul Fruitful <fruitful2007@outlook.com>
2023-04-17 23:15:47 +0100

    Correction

--- Kadane Algorithm/maximumSubArray.py
+++ Kadane Algorithm/maximumSubArray.py
@@ -2,7 +2,7 @@ def maxSub(arr):
     maxGlobal=arr[0]
     current=maxGlobal
     for i in range(1,len(arr)):
-        current=max(current,current+arr[i])
+        current=max(arr[i],current+arr[i])
         if current>maxGlobal:
             maxGlobal=current
     return maxGlobal
