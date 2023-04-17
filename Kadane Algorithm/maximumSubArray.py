93d0292b24d58d40a9f9a041fb837c1ee2b541b9
Paul Fruitful <fruitful2007@outlook.com>
2023-04-17 21:27:46 +0100

    Kadane Algo

--- Kadane Algorithm/maximumSubArray.py
+++ Kadane Algorithm/maximumSubArray.py
@@ -2,4 +2,7 @@ def maxSub(arr):
     maxGlobal=arr[0]
     current=maxGlobal
     for i in range(1,len(arr)):
-        current=arr[i]
+        current=max(current,current+arr[i])
+        if current>maxGlobal:
+            maxGlobal=current
+    return maxGlobal
