db147723be67756244a8253448504e048cdcec06
Paul Fruitful <fruitful2007@outlook.com>
2023-04-23 22:37:11 +0100

    Reduce node index

--- Linear Data Structures/linkedlist.py
+++ Linear Data Structures/linkedlist.py
@@ -37,9 +37,12 @@ class SinglyLinked:
       return ans
    def remove(self,index):
       self.getIndex(index+1).delete()
-      data=self.getIndex(self.length-(index+1))
+      data=self.list
       self.length-=1
       while data:
+         if data.index<index:
+            break
+         
          data.index-=1
          data=data.next
 
