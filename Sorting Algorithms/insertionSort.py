fa99d83f4c1fb02966ab8729f20693e448c94548
Paul Fruitful <fruitful2007@outlook.com>
2023-04-30 21:49:25 +0100

    .........

--- Sorting Algorithms/insertionSort.py
+++ Sorting Algorithms/insertionSort.py
@@ -2,16 +2,16 @@ def insertSort(array):
 
     for i in range(1, len(array)):
       current=array[i]
-      prev=i-1
+      prev=i-1 //8
       while prev>=0 and array[prev] >current:
          array[prev+1]= array[prev]
-         prev-=1
+         prev-=1 //-1
       array[prev+1]=current
     return array
 
 
 
-a=[6,8,3,2,1,20,14,35,67,23,7]
+a=[8,6]
 
 insertSort(a)
 print(a)
