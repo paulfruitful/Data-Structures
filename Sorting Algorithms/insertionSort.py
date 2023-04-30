5be764f9f95dc2f71a93d7ab4432c5afaa10e771
Paul Fruitful <fruitful2007@outlook.com>
2023-04-30 21:44:36 +0100

    Error fix

--- Sorting Algorithms/insertionSort.py
+++ Sorting Algorithms/insertionSort.py
@@ -4,10 +4,12 @@ def insertSort(array):
       current=array[i]
       prev=i-1
       while prev>=0 and array[prev] >current:
-         array[i+1]= array[prev]
+         array[prev+1]= array[prev]
          prev-=1
       array[prev+1]=current
     return array
+
+
 
 a=[6,8,3,2,1,20,14,35,67,23,7]
 
