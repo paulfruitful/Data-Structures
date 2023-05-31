eed267cc3e9c4020238729e95dacd949674717ec
Paul Fruitful <fruitful2007@outlook.com>
2023-05-31 21:02:44 +0100

    ++++

--- Sorting Algorithms/quickSort.py
+++ Sorting Algorithms/quickSort.py
@@ -1,3 +1,16 @@
+def choosePivot(a):
+   pivot=(len(a)-1)/2
+   return pivot
+def partition(a, l,r):
+   pivot=choosePivot(a)
+   leftmark=l+1
+   return leftmark
+
+
+
 def quicksort(arr, left,right):
    while(left<right):
-      pivot=0
+      pivot=partition(arr,left,right)
+      
+
+
