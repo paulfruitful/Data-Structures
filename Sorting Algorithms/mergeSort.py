7ec698b20593d1ceadae5afebf89e7d8303ff17b
Paul Fruitful <fruitful2007@outlook.com>
2023-06-11 21:48:17 +0100

    New technique

--- Sorting Algorithms/mergeSort.py
+++ Sorting Algorithms/mergeSort.py
@@ -1,10 +1,13 @@
 def mergeSort(arr,left, right):
     if left<right:
         mid=(left+right)//2
-        
+        leftArray=arr[:mid]
+        rightArray=arr[mid:len(arr)]
         mergeSort(arr,left,mid)
         mergeSort(arr,mid+1,right)
-        merge(arr, left,mid, right)
+        merge(arr, leftArray, rightArray)
 
-def merge(arr,left,mid,right):
-     barr=[0]*len(arr)
+def merge(arr,left,right):
+     i,j=0,0
+     while(i<len(left) and j<len(right)):
+         #fkk
