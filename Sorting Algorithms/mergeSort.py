240a32f3d45207f4e4d761f14da8c75a078009f0
Paul Fruitful <fruitful2007@outlook.com>
2023-06-11 22:11:19 +0100

    bug

--- Sorting Algorithms/mergeSort.py
+++ Sorting Algorithms/mergeSort.py
@@ -7,21 +7,27 @@ def mergeSort(arr,left, right):
         mergeSort(arr,mid+1,right)
         merge(arr, leftArray, rightArray)
 
-def merge(arr,left,right):
-     i,j=0,0
-     while(i<len(left) and j<len(right)):
-         if(left[i]<right[j]):
-             arr[i]=left[i]
-             i+=1
-         else:
-             arr[j]=right[j]
-             j+=1
-     while(i<len(left)):
-         arr[i]=left[i]
-         i+=1
-     while(j<len(right)):
-         arr[j]=right[j]
-         j+=1
+def merge(arr, left, right):
+    i, j, k = 0, 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
     
 test=[25,31,4,8,123,10,1,-23,9,100,34]
 mergeSort(test,0,len(test)-1)
