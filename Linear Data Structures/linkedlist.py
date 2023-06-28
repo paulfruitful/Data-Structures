a9fc0466f9f5113bbdf00f6b4a21870bc9778137
Paul Fruitful <fruitful2007@outlook.com>
2023-06-28 19:51:07 +0100

    ☑ Singly linked video

--- Linear Data Structures/linkedlist.py
+++ Linear Data Structures/linkedlist.py
@@ -3,3 +3,9 @@ class Node:
         self.value=value
         self.next=next
         self.index=index
+
+class SinglyLinked:
+   def __init__(self):
+       self.list=null
+       self.length=0
+           
