e675912dc075bdfed62b9ad77d61c20629ef0e87
Paul Fruitful <fruitful2007@outlook.com>
2023-07-13 16:32:32 +0100

    Finding the bug ...

--- Linear Data Structures/HashMap.py
+++ Linear Data Structures/HashMap.py
@@ -2,14 +2,15 @@
 
 class HashMap:
     def __init__(self,size):
-        self.size=[0*size]
-
+        self.size=[0] * size
+    
     def hasher(self,arg):
-        return arg%len(self.size)
-
+        return arg % len(self.size)
+    
         
 
 
 test=HashMap(10)
 
 
+print(test.hasher(10))
