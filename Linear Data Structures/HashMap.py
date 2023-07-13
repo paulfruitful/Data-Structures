ed71b13c65e46bae11cf4998925c7662db31bb1d
Paul Fruitful <fruitful2007@outlook.com>
2023-07-13 16:15:44 +0100

    Min Implement

--- Linear Data Structures/HashMap.py
+++ Linear Data Structures/HashMap.py
@@ -1,4 +1,15 @@
+#The HashMap Direct Chaining Implement
+
 class HashMap:
     def __init__(self,size):
         self.size=[0*size]
+
+    def hasher(self,arg):
+        return arg%len(self.size)
+
         
+
+
+test=HashMap(10)
+
+
